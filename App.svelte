1c58139239d81840dd88da3e948f0682644463b5
orbit1123 <36947660+orbit1123@users.noreply.github.com>
2019-05-30 01:16:40 +0300

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Controls from "./components/Controls.svelte";
+  import FpsMonitor from "./components/FpsMonitor.svelte";
+  import GameField from "./components/GameField.svelte";
+  import Modal from "./components/Modal.svelte";
+  import { isPlaying } from "./stores/game";
+</script>
+
+<style>
+  :global(html) {
+    height: 100%;
+  }
+  :global(body) {
+    height: 100%;
+    overscroll-behavior: none;
+    user-select: none;
+    margin: 0;
+    background-color: #efefef;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+</style>
+
+<GameField />
+<FpsMonitor />
+<Controls />
+<div>
+  {#if !$isPlaying}
+    <Modal />
+  {/if}
+</div>
